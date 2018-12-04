ee3d283c212d61c968ce0c1a5ad7c36bab5a74ec
sky2464 <sky2464@gmail.com>
2018-12-04 14:53:24 -0500

    contact us

--- travelapp/src/App.js
+++ travelapp/src/App.js
@@ -1,25 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+// const url = "https://simple-api-yeti.herokuapp.com";
+const url = "http://localhost:3001";
 
 class App extends Component {
+	state = {
+		email: "test@koidja.com",
+		name: "Julien",
+		message: "Hi I would like to have a new website!"
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		// Let's create the user object
+		const { email, name, message } = this.state;
+		const user = {
+			email,
+			name,
+			message
+		}
+		// Make the request to the email api
+		axios.post(`${ url }/email/new`, {
+			user: user
+		})
+			.then( (response) => {
+				console.log(response.data)
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <div className="col-lg-6 offset-lg-3">
+					<h1>Email Form API</h1>
+					<form onSubmit={ this.handleSubmit }>
+						<div className="form-group">
+							<label htmlFor="email">Email:</label>
+							<input
+								name="email"
+								value={ this.state.email }
+								onChange={ this.handleChange }
+								id="email"
+								type="email"
+								className="form-control"
+								required/>
+						</div>
+
+						<div className="form-group">
+							<label htmlFor="name">Name:</label>
+							<input
+								name="name"
+								value={ this.state.name }
+								onChange={ this.handleChange }
+								id="name"
+								type="text"
+								className="form-control"
+								required/>
+						</div>
+
+						<div className="form-group">
+							<label htmlFor="message">Message</label>
+							<textarea
+								name="message"
+								value={ this.state.message }
+								onChange={ this.handleChange }
+								id="message"
+								type="text"
+								className="form-control"
+								required/>
+						</div>
+
+						<button className="btn btn-primary">Submit</button>
+					</form>
+				</div>
       </div>
     );
   }
